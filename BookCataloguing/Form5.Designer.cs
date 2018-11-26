990e1549e3908cb84ca6ca6d0d15d2260367a7a3
MissJP <MissJP@LAPTOP-ABSFVM19>
2018-11-27 01:38:16 +0530

    .

--- BookCataloguing/Form5.Designer.cs
+++ BookCataloguing/Form5.Designer.cs
@@ -91,10 +91,11 @@
             this.comboBox1.DataSource = this.awardsBindingSource;
             this.comboBox1.DisplayMember = "awards";
             this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(33, 94);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(244, 21);
+            this.comboBox1.Size = new System.Drawing.Size(244, 27);
             this.comboBox1.TabIndex = 28;
             this.comboBox1.ValueMember = "awards";
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
--- BookCataloguing/Form8.cs
+++ BookCataloguing/Form8.cs
@@ -26,51 +26,58 @@ namespace BookCataloguing
 
         private void button1_Click(object sender, EventArgs e)
         {
-            SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Database1.mdf;Integrated Security=True");
-            SqlCommand cmd = new SqlCommand("insert_SP", con);
-            cmd.CommandType = CommandType.StoredProcedure;
-            cmd.Parameters.AddWithValue("@bid", textBox14.Text);
-            cmd.Parameters.AddWithValue("@bname", textBox1.Text);
-            cmd.Parameters.AddWithValue("@bimgurl", textBox4.Text);
-            cmd.Parameters.AddWithValue("@bpub", textBox9.Text);
-            cmd.Parameters.AddWithValue("@bpubyr", textBox10.Text);
-            cmd.Parameters.AddWithValue("@breview", textBox8.Text);
-            cmd.Parameters.AddWithValue("@rating", textBox2.Text);
-            cmd.Parameters.AddWithValue("@authimgurl", textBox7.Text);
-            cmd.Parameters.AddWithValue("@authname", textBox6.Text);
-            cmd.Parameters.AddWithValue("@awards", textBox5.Text);
-            cmd.Parameters.AddWithValue("@gname", textBox3.Text);
-            cmd.Parameters.AddWithValue("@agepref", textBox13.Text);
-            cmd.Parameters.AddWithValue("@buylink", textBox12.Text);
-            cmd.Parameters.AddWithValue("@location", textBox11.Text);
-            //  cmd.Parameters.AddWithValue("@", maskedTextBox13.Text);
-            //  cmd.Parameters.AddWithValue("@rating", maskedTextBox13.Text);
-            float rat=float.Parse(textBox2.Text);
-            if (rat>5)
-            {
-                MessageBox.Show("ENTER RATING LESS THAN 5");
-            }
-
-            con.Open();
             try
             {
+                SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Database1.mdf;Integrated Security=True");
+                SqlCommand cmd = new SqlCommand("insert_SP", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@bid", textBox14.Text);
+                cmd.Parameters.AddWithValue("@bname", textBox1.Text);
+                cmd.Parameters.AddWithValue("@bimgurl", textBox4.Text);
+                cmd.Parameters.AddWithValue("@bpub", textBox9.Text);
+                cmd.Parameters.AddWithValue("@bpubyr", textBox10.Text);
+                cmd.Parameters.AddWithValue("@breview", textBox8.Text);
+                cmd.Parameters.AddWithValue("@rating", textBox2.Text);
+                cmd.Parameters.AddWithValue("@authimgurl", textBox7.Text);
+                cmd.Parameters.AddWithValue("@authname", textBox6.Text);
+                cmd.Parameters.AddWithValue("@awards", textBox5.Text);
+                cmd.Parameters.AddWithValue("@gname", textBox3.Text);
+                cmd.Parameters.AddWithValue("@agepref", textBox13.Text);
+                cmd.Parameters.AddWithValue("@buylink", textBox12.Text);
+                cmd.Parameters.AddWithValue("@location", textBox11.Text);
+                //  cmd.Parameters.AddWithValue("@", maskedTextBox13.Text);
+                //  cmd.Parameters.AddWithValue("@rating", maskedTextBox13.Text);
+                float rat = float.Parse(textBox2.Text);
                 if (rat > 5)
                 {
-                   // Form8 f8 = new Form8();
                     MessageBox.Show("ENTER RATING LESS THAN 5");
-                    
-                    new Form8().ShowDialog();
-                    this.Close();
-                    return;
                 }
-                cmd.ExecuteNonQuery();
-                this.Close();
+
+                con.Open();
+                try
+                {
+                    if (rat > 5)
+                    {
+                        // Form8 f8 = new Form8();
+                        MessageBox.Show("ENTER RATING LESS THAN 5");
+
+                        new Form8().ShowDialog();
+                        this.Close();
+                        return;
+                    }
+                    cmd.ExecuteNonQuery();
+                    this.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("    <<<<<INVALID SQL OPERATION>>>\n" + ex);
+                }
+                con.Close();
             }
-            catch (Exception ex)
+            catch(Exception te)
             {
-                MessageBox.Show("    <<<<<INVALID SQL OPERATION>>>\n" + ex);
+                MessageBox.Show("Enter the details properly");
             }
-            con.Close();
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
